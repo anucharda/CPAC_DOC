841be6e0bd08a60d4416cdb0acf9d535f4a99231
Sirirat Sakoncharoengit <siriratst@gmail.com>
2017-04-02 23:56:10 +0700

    no message

---
+++ Batch/Deploy/script db/5-create table CL_TMP_EXEMPT_CREDIT_LIMIT.sql
@@ -0,0 +1,30 @@
+
+CREATE TABLE dbo.CL_TMP_EXEMPT_CREDIT_LIMIT ( 
+	TMP_ID           	UNSIGNED BIGINT IDENTITY NOT NULL,
+    EXEMPT_CUSTOMER_ID               UNSIGNED BIGINT  NOT NULL,
+    CA_NO               varchar(30) NOT NULL,
+    BA_NO               varchar(30) NOT NULL,
+	MOBILE_NO           varchar(30) NOT NULL,
+	EXEMPT_MODE         varchar(30) NOT NULL,
+	EXEMPT_LEVEL        varchar(30) NOT NULL,	
+	CHANNEL            	varchar(30) NOT NULL,	
+	EFFECTIVE_DATE      datetime NOT NULL,
+	END_DATE            datetime NOT NULL,
+	EXPIRED_DATE        datetime NOT NULL,
+	DURATION            int NOT NULL,	
+	LOCATION_CODE		varchar(30) NOT NULL,	
+	REASON				varchar(30) NOT NULL,	
+    WORK_ORDER_ID       varchar(50) NULL,
+    GEN_FLAG            char(1) DEFAULT     'N' NOT NULL,
+    GEN_DATETIME        datetime  NULL,
+	CONSTRAINT PK_CL_TMP_EXEMPT_CREDIT_LIMIT PRIMARY KEY CLUSTERED(TMP_ID)
+	WITH max_rows_per_page = 0, reservepagegap = 0
+	)
+LOCK ALLPAGES
+WITH max_rows_per_page = 0, reservepagegap = 0, identity_gap = 10
+ON [default] 
+GO
+CREATE NONCLUSTERED INDEX IND_CL_TMP_EXEMPT_CREDIT_LIMIT_WORK_ORDER_ID
+	ON dbo.CL_TMP_EXEMPT_CREDIT_LIMIT(WORK_ORDER_ID)
+	ON [default]
+GO
--- Batch/Example Real Data  (DB&File)/Activity Log/SMS/insert.sql
+++
@@ -1,3 +0,0 @@
-SET IDENTITY_INSERT dbo.CL_SMS ON
-insert into CLDB.dbo.CL_SMS
-select * from CLDB_ok1.dbo.CL_SMS
